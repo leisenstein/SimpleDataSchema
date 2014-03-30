1a7e996c1c25408439283efec357262aec65b4ce
Larry Eisenstein <larryeisenstein@gmail.com>
2014-03-30 15:08:10 -0400

    Create DBSeed-0.1.sql

---
+++ DBSeed-0.1.sql
@@ -0,0 +1,74 @@
+insert into states (Id, StateName) values (1,'Alabama');
+insert into states (Id, StateName) values (2,'Alaska');
+insert into states (Id, StateName) values (3,'American Samoa');
+insert into states (Id, StateName) values (4,'Arizona');
+insert into states (Id, StateName) values (5,'Arkansas');
+insert into states (Id, StateName) values (6,'California');
+insert into states (Id, StateName) values (7,'Colorado');
+insert into states (Id, StateName) values (8,'Connecticut');
+insert into states (Id, StateName) values (9,'Delaware');
+insert into states (Id, StateName) values (10,'District of Columbia');
+insert into states (Id, StateName) values (11,'Federated States of Micronesia');
+insert into states (Id, StateName) values (12,'Florida');
+insert into states (Id, StateName) values (13,'Georgia');
+insert into states (Id, StateName) values (14,'Guam');
+insert into states (Id, StateName) values (15,'Hawaii');
+insert into states (Id, StateName) values (16,'Idaho');
+insert into states (Id, StateName) values (17,'Illinois');
+insert into states (Id, StateName) values (18,'Indiana');
+insert into states (Id, StateName) values (19,'Iowa');
+insert into states (Id, StateName) values (20,'Kansas');
+insert into states (Id, StateName) values (21,'Kentucky');
+insert into states (Id, StateName) values (22,'Louisiana');
+insert into states (Id, StateName) values (23,'Maine');
+insert into states (Id, StateName) values (24,'Marshall Islands');
+insert into states (Id, StateName) values (25,'Maryland');
+insert into states (Id, StateName) values (26,'Massachusetts');
+insert into states (Id, StateName) values (27,'Michigan');
+insert into states (Id, StateName) values (28,'Minnesota');
+insert into states (Id, StateName) values (29,'Mississippi');
+insert into states (Id, StateName) values (30,'Missouri');
+insert into states (Id, StateName) values (31,'Montana');
+insert into states (Id, StateName) values (32,'Nebraska');
+insert into states (Id, StateName) values (33,'Nevada');
+insert into states (Id, StateName) values (34,'New Hampshire');
+insert into states (Id, StateName) values (35,'New Jersey');
+insert into states (Id, StateName) values (36,'New Mexico');
+insert into states (Id, StateName) values (37,'New York');
+insert into states (Id, StateName) values (38,'North Carolina');
+insert into states (Id, StateName) values (39,'North Dakota');
+insert into states (Id, StateName) values (40,'Northern Mariana Islands');
+insert into states (Id, StateName) values (41,'Ohio');
+insert into states (Id, StateName) values (42,'Oklahoma');
+insert into states (Id, StateName) values (43,'Oregon');
+insert into states (Id, StateName) values (44,'Palau');
+insert into states (Id, StateName) values (45,'Pennsylvania');
+insert into states (Id, StateName) values (46,'Puerto Rico');
+insert into states (Id, StateName) values (47,'Rhode Island');
+insert into states (Id, StateName) values (48,'South Carolina');
+insert into states (Id, StateName) values (49,'South Dakota');
+insert into states (Id, StateName) values (50,'Tennessee');
+insert into states (Id, StateName) values (51,'Texas');
+insert into states (Id, StateName) values (52,'Utah');
+insert into states (Id, StateName) values (53,'Vermont');
+insert into states (Id, StateName) values (54,'Virgin Islands');
+insert into states (Id, StateName) values (55,'Virginia');
+insert into states (Id, StateName) values (56,'Washington');
+insert into states (Id, StateName) values (57,'West Virginia');
+insert into states (Id, StateName) values (58,'Wisconsin');
+insert into states (Id, StateName) values (59,'Wyoming');
+
+INSERT INTO Contacts VALUES('Michael', 'Jordan', 'michael@bulls.com', 'Chicago Bulls', 'MVP of Chicago Bulls')
+INSERT INTO Contacts VALUES('LaBron', 'james', 'labron@heat.com', 'Miami Heat', 'King James')
+INSERT INTO Contacts VALUES('Kobe', 'Bryant', 'kobe@lakers.com', 'Los Angeles Lakers', 'The Dagger')
+INSERT INTO Contacts VALUES('Kevin', 'Durant', 'kevin@thunder.com', 'Oklahoma Thunder', 'The Durantula')
+INSERT INTO Contacts VALUES('Kyrie', 'Irving', 'kyrie@cavs.com', 'Cleveland Cavaliers', 'Duke Knight')
+INSERT INTO Contacts VALUES('Chris', 'Paul', 'chris@clippers.com', 'Los Angeles Clippers', 'CP3')
+
+INSERT INTO Addresses VALUES(1, 'Home', '123 Main Street', 'Chicago', 17, '60290')
+INSERT INTO Addresses VALUES(1, 'Work', '1901 West Madison St', 'Chicago', 17, '60612')
+INSERT INTO Addresses VALUES(2, 'Home', '123 Billing Street', 'Maimi', 12, '60290')
+INSERT INTO Addresses VALUES(3, 'Home', '1152 Temptation Street', 'Los Angeles', 6, '90001')
+INSERT INTO Addresses VALUES(4, 'Home', '234 Umbridge Rd', 'Oklahoma City', 42, '73101')
+INSERT INTO Addresses VALUES(5, 'Home', '2211 Miracle Street', 'Cleveland', 41, '44101')
+INSERT INTO Addresses VALUES(6, 'Home', '97663 Johns Ave', 'Los Angeles', 6, '90005')
